09cbd0693b9f712abc466f43eec558ec1884a195
SolarAA-11 <948872263@qq.com>
2024-06-24 14:15:14 +0800

    添加 Goolge Test

--- Leetcode/leetcode0002.cpp
+++ Leetcode/leetcode0002.cpp
@@ -1,5 +1,3 @@
-#include <gtest/gtest.h>
-
 #include <iostream>
 
 #include "lc_helper.h"
@@ -9,15 +7,13 @@ using namespace lc;
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* pResultNodeList{0};
+        while(l1 != nullptr && l2 != nullptr) {
 
+        }
     }
 };
 
 int main() {
-    ASSERT_EQ(1, 1);
-    std::cout << "hello" << std::endl;
-    ASSERT_EQ(1, 2);
-    std::cout << "hello" << std::endl;
-
     return 0;
 }
