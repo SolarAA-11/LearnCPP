cbe7a838ae57f0803e7017a78437083ba77227e7
SolarAA-11 <948872263@qq.com>
2024-07-26 18:18:49 +0800

    inlinE CONSTANT

---
+++ LearnCPPSerials/Chapter07/inlineConstantGlobalVariable-Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "inlineConstantGlobalVariable-Main.hpp"
+
+int main()
+{
+    std::cout << "Enter a radius: ";
+    double radius{};
+    std::cin >> radius;
+
+    std::cout << "The circumference is: " << 2 * radius * constants::pi << '\n';
+
+    return 0;
+}
